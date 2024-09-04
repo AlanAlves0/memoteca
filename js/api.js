442db489a0fb17c59bcfdb53d45adf7c7f9ab818
Alan Alves <125587710+AlanAlves0@users.noreply.github.com>
2024-09-04 15:01:25 -0300

    adicionando o PUT para modificar

--- js/api.js
+++ js/api.js
@@ -22,11 +22,38 @@ const api = {
             return await response.json() 
         }
         catch{
+            alert("Erro ao salvar pensamentos")
+            throw error
+        }
+    },
+
+    async buscarPensamentosPorId(id){
+        try{
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+            return await response.json() 
+        }
+        catch{
             alert("Erro ao buscar pensamentos")
             throw error
         }
-    }
+    },
 
+    async editarPensamento(pensamento){
+        try{
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-type":"application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json() 
+        }
+        catch{
+            alert("Erro ao editar pensamentos")
+            throw error
+        }
+    },
 
 }
 
