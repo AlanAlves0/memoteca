795c3af7dc25f8b64480fb9b15ca98a6c8253c4a
Alan Alves <125587710+AlanAlves0@users.noreply.github.com>
2024-09-04 14:20:25 -0300

    adicionando GET e POST

--- js/api.js
+++ js/api.js
@@ -13,7 +13,11 @@ const api = {
     async salvarPensamento(pensamento){
         try{
             const response = await fetch('http://localhost:3000/pensamentos', {
-                method: "POST"
+                method: "POST",
+                headers: {
+                    "Content-type":"application/json"
+                },
+                body: JSON.stringify(pensamento)
             })
             return await response.json() 
         }
