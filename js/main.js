8b222c161fb3266bf6e168eef865a08c988320e8
Alan Alves <125587710+AlanAlves0@users.noreply.github.com>
2024-09-04 15:32:37 -0300

    update

--- js/main.js
+++ js/main.js
@@ -2,44 +2,34 @@ import ui from "./ui.js"
 import api from "./api.js"
 
 document.addEventListener("DOMContentLoaded", () => {
-    ui.redenrizarPensamentos()
-    
-    const formularioPensamento = document.getElementById("pensamento-form")
-    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+  ui.renderizarPensamentos()
+
+  const formularioPensamento = document.getElementById("pensamento-form")
+  const botaoCancelar = document.getElementById("botao-cancelar")
+
+  formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+  botaoCancelar.addEventListener("click", manipularCancelamento)
 })
 
 async function manipularSubmissaoFormulario(event) {
-    event.preventDefault()
-    const id = document.getElementById("pensamento-id").value
-    const conteudo = document.getElementById("pensamento-conteudo").value
-    const autoria = document.getElementById("pensamento-autoria").value
-    
-    try{
-        if(id){
-            await api.editarPensamento({id, conteudo, autoria})
-        }else{
-            await api.salvarPensamento({ conteudo, autoria })
-        }
-        ui.redenrizarPensamentos()
-       
+  event.preventDefault()
+  const id = document.getElementById("pensamento-id").value
+  const conteudo = document.getElementById("pensamento-conteudo").value
+  const autoria = document.getElementById("pensamento-autoria").value
+
+  try { 
+    if(id) {
+      await api.editarPensamento({ id, conteudo, autoria })
+    } else {
+      await api.salvarPensamento({ conteudo, autoria })
     }
-    catch {
-        alert("Erro ao salvar pensamento")
-    }
+    ui.renderizarPensamentos()
+  }
+  catch {
+    alert("Erro ao salvar pensamento")
+  }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    ui.redenrizarPensamentos()
-  
-    const formularioPensamento = document.getElementById("pensamento-form")
-    const botaoCancelar = document.getElementById("botao-cancelar")
-  
-    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
-    botaoCancelar.addEventListener("click", manipularCancelamento)
-  })
-  
-  //código omitido
-  
-  function manipularCancelamento() {
-    ui.limparFormulario();
-  }
+function manipularCancelamento() {
+  ui.limparFormulario()
+}
